80970c8c596d9d9819912e0dd0dfd4420d384ebe
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2023-01-29 23:44:33 +0900

    feat: add other answer

--- chapter1/practice1-3e.sh
+++ chapter1/practice1-3e.sh
@@ -1,2 +1,8 @@
 seq 5 | awk '{print $1%2?"奇数":"偶数"}' | sort -r | uniq -c
 seq 5 | awk '{print $1%2?"奇数":"偶数"}' | sort -r | uniq -c | awk '{print $2,$1}'
+
+# ソートオプション
+seq 5 | awk '{print $1%2?"奇数":"偶数"}' | sort | uniq -c | awk '{print $2,$1}' | sort -k2,2n
+
+# 別解
+seq 5 | awk '{print$1%2?"奇数":"偶数"}' | awk '{a[$1]++}END{for(k in a)print k,a[k]}'
